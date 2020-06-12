09c573594a1f68082aff271fd3cc21e9bb1e3ebb
Emanuel1712bia <50499787+Emanuel1712bia@users.noreply.github.com>
2020-06-12 19:36:24 -0300

    Forms
    CRUd

---
+++ CadastroFuncionário.Designer.cs
@@ -0,0 +1,378 @@
+﻿namespace NetCoreForms
+{
+    partial class CadastroFuncionário
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblSalario = new System.Windows.Forms.Label();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.lblErros = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.rbMasculino = new System.Windows.Forms.RadioButton();
+            this.rbFeminino = new System.Windows.Forms.RadioButton();
+            this.rbCLT = new System.Windows.Forms.RadioButton();
+            this.rbPJ = new System.Windows.Forms.RadioButton();
+            this.rbAutonomo = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.rbAtivo = new System.Windows.Forms.RadioButton();
+            this.rbInativo = new System.Windows.Forms.RadioButton();
+            this.txtCargo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDepartamento = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.maskedtxtCPFCNPJ = new System.Windows.Forms.MaskedTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(23, 9);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(38, 13);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "&Nome:";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(26, 78);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(35, 13);
+            this.lblEmail.TabIndex = 1;
+            this.lblEmail.Text = "&Email:";
+            // 
+            // lblSalario
+            // 
+            this.lblSalario.AutoSize = true;
+            this.lblSalario.Location = new System.Drawing.Point(27, 142);
+            this.lblSalario.Name = "lblSalario";
+            this.lblSalario.Size = new System.Drawing.Size(42, 13);
+            this.lblSalario.TabIndex = 4;
+            this.lblSalario.Text = "&Salário:";
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(27, 393);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(95, 32);
+            this.btnSalvar.TabIndex = 6;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.SalvarAction);
+            // 
+            // lblErros
+            // 
+            this.lblErros.AutoSize = true;
+            this.lblErros.BackColor = System.Drawing.Color.White;
+            this.lblErros.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblErros.ForeColor = System.Drawing.Color.Red;
+            this.lblErros.Location = new System.Drawing.Point(23, 247);
+            this.lblErros.Name = "lblErros";
+            this.lblErros.Size = new System.Drawing.Size(0, 13);
+            this.lblErros.TabIndex = 7;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(27, 25);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(240, 20);
+            this.txtNome.TabIndex = 8;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(28, 105);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(240, 20);
+            this.txtEmail.TabIndex = 9;
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Location = new System.Drawing.Point(27, 159);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(100, 20);
+            this.txtSalario.TabIndex = 10;
+            // 
+            // rbMasculino
+            // 
+            this.rbMasculino.AutoSize = true;
+            this.rbMasculino.Checked = true;
+            this.rbMasculino.Location = new System.Drawing.Point(12, 20);
+            this.rbMasculino.Name = "rbMasculino";
+            this.rbMasculino.Size = new System.Drawing.Size(73, 17);
+            this.rbMasculino.TabIndex = 11;
+            this.rbMasculino.TabStop = true;
+            this.rbMasculino.Text = "&Masculino";
+            this.rbMasculino.UseVisualStyleBackColor = true;
+            // 
+            // rbFeminino
+            // 
+            this.rbFeminino.AutoSize = true;
+            this.rbFeminino.Location = new System.Drawing.Point(100, 20);
+            this.rbFeminino.Name = "rbFeminino";
+            this.rbFeminino.Size = new System.Drawing.Size(67, 17);
+            this.rbFeminino.TabIndex = 12;
+            this.rbFeminino.TabStop = true;
+            this.rbFeminino.Text = "&Feminino";
+            this.rbFeminino.UseVisualStyleBackColor = true;
+            // 
+            // rbCLT
+            // 
+            this.rbCLT.AutoSize = true;
+            this.rbCLT.Checked = true;
+            this.rbCLT.Location = new System.Drawing.Point(29, 35);
+            this.rbCLT.Name = "rbCLT";
+            this.rbCLT.Size = new System.Drawing.Size(45, 17);
+            this.rbCLT.TabIndex = 13;
+            this.rbCLT.TabStop = true;
+            this.rbCLT.Text = "&CLT";
+            this.rbCLT.UseVisualStyleBackColor = true;
+            // 
+            // rbPJ
+            // 
+            this.rbPJ.AutoSize = true;
+            this.rbPJ.Location = new System.Drawing.Point(120, 35);
+            this.rbPJ.Name = "rbPJ";
+            this.rbPJ.Size = new System.Drawing.Size(37, 17);
+            this.rbPJ.TabIndex = 14;
+            this.rbPJ.TabStop = true;
+            this.rbPJ.Text = "&PJ";
+            this.rbPJ.UseVisualStyleBackColor = true;
+            // 
+            // rbAutonomo
+            // 
+            this.rbAutonomo.AutoSize = true;
+            this.rbAutonomo.Location = new System.Drawing.Point(211, 35);
+            this.rbAutonomo.Name = "rbAutonomo";
+            this.rbAutonomo.Size = new System.Drawing.Size(73, 17);
+            this.rbAutonomo.TabIndex = 15;
+            this.rbAutonomo.TabStop = true;
+            this.rbAutonomo.Text = "&Autônomo";
+            this.rbAutonomo.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbCLT);
+            this.groupBox1.Controls.Add(this.rbAutonomo);
+            this.groupBox1.Controls.Add(this.rbPJ);
+            this.groupBox1.Location = new System.Drawing.Point(289, 117);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(400, 80);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "&Tipo de Contrato";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rbMasculino);
+            this.groupBox2.Controls.Add(this.rbFeminino);
+            this.groupBox2.Location = new System.Drawing.Point(289, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(400, 80);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "&Sexo";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(799, 22);
+            this.statusStrip1.TabIndex = 18;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(39, 17);
+            this.toolStripStatusLabel1.Text = "Status";
+            this.toolStripStatusLabel1.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.rbAtivo);
+            this.groupBox3.Controls.Add(this.rbInativo);
+            this.groupBox3.Location = new System.Drawing.Point(25, 281);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(180, 47);
+            this.groupBox3.TabIndex = 19;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "&Ativo";
+            // 
+            // rbAtivo
+            // 
+            this.rbAtivo.AutoSize = true;
+            this.rbAtivo.Checked = true;
+            this.rbAtivo.Location = new System.Drawing.Point(12, 20);
+            this.rbAtivo.Name = "rbAtivo";
+            this.rbAtivo.Size = new System.Drawing.Size(49, 17);
+            this.rbAtivo.TabIndex = 11;
+            this.rbAtivo.TabStop = true;
+            this.rbAtivo.Text = "Ativo";
+            this.rbAtivo.UseVisualStyleBackColor = true;
+            // 
+            // rbInativo
+            // 
+            this.rbInativo.AutoSize = true;
+            this.rbInativo.Location = new System.Drawing.Point(100, 20);
+            this.rbInativo.Name = "rbInativo";
+            this.rbInativo.Size = new System.Drawing.Size(57, 17);
+            this.rbInativo.TabIndex = 12;
+            this.rbInativo.TabStop = true;
+            this.rbInativo.Text = "&Inativo";
+            this.rbInativo.UseVisualStyleBackColor = true;
+            // 
+            // txtCargo
+            // 
+            this.txtCargo.Location = new System.Drawing.Point(25, 347);
+            this.txtCargo.Name = "txtCargo";
+            this.txtCargo.Size = new System.Drawing.Size(242, 20);
+            this.txtCargo.TabIndex = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 331);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "&Cargo:";
+            // 
+            // txtDepartamento
+            // 
+            this.txtDepartamento.Location = new System.Drawing.Point(289, 347);
+            this.txtDepartamento.Name = "txtDepartamento";
+            this.txtDepartamento.Size = new System.Drawing.Size(242, 20);
+            this.txtDepartamento.TabIndex = 22;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(286, 331);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "&Departamento";
+            // 
+            // maskedtxtCPFCNPJ
+            // 
+            this.maskedtxtCPFCNPJ.Location = new System.Drawing.Point(301, 235);
+            this.maskedtxtCPFCNPJ.Name = "maskedtxtCPFCNPJ";
+            this.maskedtxtCPFCNPJ.Size = new System.Drawing.Size(177, 20);
+            this.maskedtxtCPFCNPJ.TabIndex = 24;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(301, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "&CPF/CNPJ";
+            // 
+            // CadastroFuncionário
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.maskedtxtCPFCNPJ);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDepartamento);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCargo);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblErros);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.lblSalario);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblNome);
+            this.Name = "CadastroFuncionário";
+            this.Text = "Cadastro de Funcionário";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblSalario;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Label lblErros;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.RadioButton rbMasculino;
+        private System.Windows.Forms.RadioButton rbFeminino;
+        private System.Windows.Forms.RadioButton rbCLT;
+        private System.Windows.Forms.RadioButton rbPJ;
+        private System.Windows.Forms.RadioButton rbAutonomo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton rbAtivo;
+        private System.Windows.Forms.RadioButton rbInativo;
+        private System.Windows.Forms.TextBox txtCargo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDepartamento;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox maskedtxtCPFCNPJ;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ CadastroFuncionário.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NetCoreForms.Modelo;
+using System.ComponentModel.DataAnnotations;
+using NetCoreForms.Banco;
+
+
+namespace NetCoreForms
+{
+    public partial class CadastroFuncionário : Form
+    {
+        private TelaPrincipal TelaPrincipal;
+        private Funcionario func;
+
+        public CadastroFuncionário(TelaPrincipal tela)
+        {
+            TelaPrincipal = tela;
+            InitializeComponent();
+            
+            
+        }
+        
+        
+        public CadastroFuncionário(TelaPrincipal tela, int Id)
+        {
+            TelaPrincipal = tela;
+            
+            InitializeComponent();
+
+            func = FuncionarioDataAccess.CarregarFuncionarios(Id);
+            CarregarFuncionarioTela(func);
+        }
+
+        private void CarregarFuncionarioTela(Funcionario funcionario)
+        {
+
+            txtNome.Text = funcionario.Nome;
+            txtEmail.Text = funcionario.Email;
+            txtCargo.Text = funcionario.Cargo;
+            txtDepartamento.Text = funcionario.Departamento;
+            txtSalario.Text = funcionario.Salario.ToString();
+            if(funcionario.Sexo == "M") { rbMasculino.Checked = true; } else { rbFeminino.Checked = true; };
+            if(funcionario.Ativo == "S") { rbAtivo.Checked = true; } else { rbInativo.Checked = true; };
+            if(funcionario.TipoContrato == "CLT") { rbCLT.Checked = true; } else if (funcionario.TipoContrato == "PJ") { rbPJ.Checked = true; } else { rbAutonomo.Checked = true; };
+
+        }
+        private void SalvarAction(object sender, EventArgs e)
+        {
+            Funcionario funcionario;
+            if (func != null)
+            {
+                //Atualização
+                funcionario = func;
+                funcionario.DataAtualizacao = DateTime.Now;
+            }
+            else
+            {
+                //Novo Cadastro
+                funcionario = new Funcionario();
+                funcionario.DataCadastro = DateTime.Now;
+
+            }
+
+            //Mover dados para a Classe Funcionario
+            funcionario.Nome = txtNome.Text.Trim();
+            funcionario.Email = txtEmail.Text.Trim();
+            funcionario.Cargo = txtCargo.Text.Trim();
+            funcionario.Departamento = txtDepartamento.Text.Trim();
+            funcionario.Salario = decimal.Parse (txtSalario.Text);
+            funcionario.Sexo = (rbMasculino.Checked) ? "M" : "F" ;
+            funcionario.Ativo = (rbAtivo.Checked) ? "S" : "N" ;
+            funcionario.TipoContrato = (rbCLT.Checked) ? "CLT" : (rbPJ.Checked) ? "PJ" : "Aut" ;
+            
+
+            //Validar os Dados
+            List<ValidationResult> listErros = new List<ValidationResult>();
+            ValidationContext contexto = new ValidationContext(funcionario);
+            bool Validar = Validator.TryValidateObject(funcionario, contexto, listErros, true);
+
+            if (Validar)
+            {
+                //Salvar os Dados
+                //Fechar e Atualizar a TelaPrincipal
+                bool result;
+                if (func != null)
+                {
+                    //Atualizar
+                    result = FuncionarioDataAccess.AtualizarFuncionario(funcionario);
+                }
+                else
+                {
+                    result = FuncionarioDataAccess.SalvarFuncionario(funcionario);
+                }
+
+                if (result)
+                {
+                    //Sucesso
+                    TelaPrincipal.AtualizarTabela();
+                    this.Close();
+                }
+                else
+                {
+                    //Erro
+                    lblErros.Text = "Não foi possível gravar os dados. Verifique!";
+                }
+            }
+            else
+            {
+                //Validar os dados.
+                StringBuilder sb = new StringBuilder();
+                foreach (ValidationResult erro in listErros)
+                {
+                    sb.Append(erro.ErrorMessage + "\n");
+                }
+                lblErros.Text = sb.ToString();
+
+            }
+            
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
